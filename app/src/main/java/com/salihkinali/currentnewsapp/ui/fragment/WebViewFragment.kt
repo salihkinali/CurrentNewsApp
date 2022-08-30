0304ae7cd53b0bf553ddff39bb7d93f46198d4a6
salihkinali <salih_3220@hotmail.com>
2022-08-30 15:58:31 +0300

    The file has been modified.

---
+++ app/src/main/java/com/salihkinali/currentnewsapp/ui/fragment/WebViewFragment.kt
@@ -0,0 +1,33 @@
+package com.salihkinali.currentnewsapp.ui.fragment
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.salihkinali.currentnewsapp.databinding.FragmentWebViewBinding
+
+class WebViewFragment : Fragment() {
+    private var _binding: FragmentWebViewBinding? = null
+    private val binding get() = _binding!!
+    private val args: WebViewFragmentArgs by navArgs()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWebViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            webViewScreen.settings.javaScriptEnabled = true
+            webViewScreen.loadUrl(args.url)
+        }
+
+    }
+}
