09a89f761aadbbbfd45340835c82c5f9463120b8
salihkinali <salih_3220@hotmail.com>
2022-10-11 23:12:23 +0300

    Added new options in settingFragment

--- app/src/main/java/com/salihkinali/currentnewsapp/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/salihkinali/currentnewsapp/ui/fragment/SettingFragment.kt
@@ -1,24 +1,48 @@
 package com.salihkinali.currentnewsapp.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.salihkinali.currentnewsapp.databinding.FragmentSettingBinding
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.salihkinali.currentnewsapp.R
 
 
-class SettingFragment : Fragment() {
-    private var _binding: FragmentSettingBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSettingBinding.inflate(inflater, container, false)
-        return binding.root
+class SettingFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference, rootKey)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
 
+    private fun initView() {
+        findPreference<Preference>("developer")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                openView("https://twitter.com/S_kinali")
+                true
+            }
+        findPreference<Preference>("source_code")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                openView("https://github.com/salihkinali/CurrentNewsApp")
+                true
+            }
+        findPreference<Preference>("send_feedback")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                openView("https://docs.google.com/forms/d/1GOUNwV12H3QDjhM26GSZRQbhV-mOXvUTbOyVlPGWOB0/")
+                true
+            }
+
+    }
+
+    private fun openView(url: String) {
+        val viewIntent = Intent(
+            "android.intent.action.VIEW",
+            Uri.parse(url)
+        )
+        startActivity(viewIntent)
+    }
 }
